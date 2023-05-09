02c65e1ae991133ca7326b4e4b2be1a4229f14b0
Ibrahim270130 <opajobiolakunle@gmail.com>
2023-05-09 16:44:55 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+from json import loads
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """ecursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return None
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+
+            }
+            response = get(url, headers=headers)
+            reddits = response.json()
+
+            try:
+            subscribers = reddits.get('data').get('subscribers')
+            return int(subscribers)
+            except:
+            return 0
